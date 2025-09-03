de863eb4787967eae58ea0637168b10e5b12c6a4
Arnav1511 <meetarnavranjan@gmail.com>
2025-09-04 04:27:18 +0530

    refactoring code and building the god mod along with it

--- CRUD/main.go
+++ CRUD/main.go
@@ -79,6 +79,8 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
+////
+
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
